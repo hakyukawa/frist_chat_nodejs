5329c5e103776c477a44df9c99a5ebd764000644
Toma <kirima--idea--@au.com>
2025-03-06 15:35:35 +0900

    [add]メッセージの取得処理を追加

--- routes/router.js
+++ routes/router.js
@@ -8,35 +8,38 @@ const validate_body = require('../middleware/validate_body');
 const server_handler = require('../handlers/server_handler');
 
 const v1 = express.Router();
+v1.use(validate_body);
 //ログイン
-v1.post('/login',validate_body, user_handler.login);    //http://localhost:3000/api/v1/login
+v1.post('/login',validate_body, user_handler.login);    // http://localhost:3001/api/v1/login
 //新規登録
-v1.post('/signup', validate_body, user_handler.signup);  //http://localhost:3000/api/v1/signup
+v1.post('/signup', validate_body, user_handler.signup);  // http://localhost:3001/api/v1/signup
 
 
 // 認証後のエンドポイント
 const auth = express.Router();
 auth.use(middleware_auth.verify_token);
-auth.use(validate_body);
 
 const user = express.Router();
 // ユーザー関連のエンドポイント
-user.get('/profile', user_handler.get_profile);
+user.get('/profile', user_handler.get_profile);         // http://localhost:3001/api/v1/user/profile
 // ユーザー情報取得
-user.get('/friendship', friend_handler.friendship);    //http://localhost:3000/api/v1/user/friendship
+user.get('/friendship', friend_handler.friendship);    // http://localhost:3001/api/v1/user/friendship
 
 // サーバー関連のエンドポイント
 const server = express.Router();
 // サーバー作成
-server.post('/create', server_handler.create_server);    //http://localhost:3000/api/v1/server/create
+server.post('/create', server_handler.create_server);    // http://localhost:3001/api/v1/server/create
 
 // チャンネル関連のエンドポイント
 const channel = express.Router();
 // チャンネル作成
-channel.post('/create', user_handler.createChannel);     //http://localhost:3000/api/v1/channel/create
+channel.post('/create', user_handler.createChannel);     // http://localhost:3001/api/v1/channel/create
 // メッセージ送信
-channel.post('/message', message_handler.send_message);    //http://localhost:3000/api/v1/channel/message
-channel.put('/message/:message_id', message_handler.edit_message);     //http://localhost:3000/api/v1/channel/message
+channel.post('/message', message_handler.send_message);    // http://localhost:3001/api/v1/channel/message
+// メッセージ編集
+channel.put('/message/:message_id', message_handler.edit_message);     // http://localhost:3001/api/v1/channel/message/:message_id
+//メッセージ取得
+channel.get('/message/:channel_id', message_handler.get_message);    // http://localhost:3001/api/v1/channel/message/:channel_id
 
 //　エンドポイントをマウント
 server.use('/channel', channel);
